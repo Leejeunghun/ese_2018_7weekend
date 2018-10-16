701484825c8f04f548040f3cf0563eabe5f59850
JeungHun Lee <201401662@inu.ac.kr>
2018-10-16 01:00:02 -0700

    add preprocess

--- myapp.c
+++ myapp.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #include"func1.h"
 #include"func2.h"
+void preprocess()
+{
+    printf("preprocess doing\n");
+}
 
 void main()
 {
+     preprocess();
      foo();
      goo();
 }	
